f9c634eaa0d8c587b621688b2dde98d2cf810083
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-07-05 19:09:39 +0300

    Deleted Payment related files

--- src/main/java/com/ahmad/studentTest/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ahmad.studentTest.repository;
-
-//import com.ahmad.studentTest.model.Payment;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PaymentRepository extends JpaRepository<Payment,Long> {
-//}
